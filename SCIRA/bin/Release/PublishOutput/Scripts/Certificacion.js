4bb71765898ed269425e1424399ffdffca30b1c0
GabolanPerez <gpelcastrep@gmail.com>
2024-04-03 03:49:05 -0600

    Eliminando Publicación dentro de Proyecto y carpeta zip AppData

--- SCIRA/bin/Release/PublishOutput/Scripts/Certificacion.js
+++
@@ -1,9 +0,0 @@
-function FileName(id) {
-    var filename = $("#f" + id).val();
-    $("#nb_a" + id).val(filename);
-}
-
-function ResetFileName(id) {
-    $("#f" + id).val("");
-    $("#nb_a" + id).val("");
-}
--- SCIRA/bin/Release/PublishOutput/Scripts/Tutorial.js
+++
@@ -1,5 +0,0 @@
-function runTutorial() {
-    enjoyhint_instance = new EnjoyHint({});
-    enjoyhint_instance.setScript(enjoyhint_script_data);
-    enjoyhint_instance.runScript();
-}
